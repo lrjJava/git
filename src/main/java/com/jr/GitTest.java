e3817f2b0644601babaaea08ff1dc4c76c98e4ce
Lrj <Lrj@atguigu.com>
2022-08-09 09:21:18 +0300

    first commit

---
+++ src/main/java/com/jr/GitTest.java
@@ -0,0 +1,13 @@
+package com.jr;
+
+/**
+ * @title: GitTest
+ * @Author Lrj
+ * @Date: 2022/8/9 9:20
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
